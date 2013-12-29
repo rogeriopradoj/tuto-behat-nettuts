b7b926b910425113b69dc18507b8047e6051f41b
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2013-12-29 07:47:25 -0200

    Create RogerioPradoJ\TutoBehatNettuts\Phpadder.php
    Signed-off-by: Rogerio Prado de Jesus <rogeriopradoj@gmail.com>

---
+++ src/RogerioPradoJ/TutoBehatNettuts/Phpadder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace RogerioPradoJ\TutoBehatNettuts;
+
+class Phpadder
+{
+    private $a;
+    private $b;
+    public $sum;
+
+    public function __construct($a, $b)
+    {
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    public function add()
+    {
+        $this->sum = $this->a = $this->b;
+    }
+
+    public function display()
+    {
+        return $this->sum;
+    }
+}
